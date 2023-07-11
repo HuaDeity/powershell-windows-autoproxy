811bd31c2b419c9d38220bd6670124b0aa5b8f68
HuaDeity <hello@huadeity.com>
2023-07-11 13:48:53 +0800

    add noproxy to init

--- powershell-windows-autoproxy.psm1
+++ powershell-windows-autoproxy.psm1
@@ -9,8 +9,8 @@ function proxy {
         [Environment]::SetEnvironmentVariable("https_proxy", "http://$proxyServer")
         [Environment]::SetEnvironmentVariable("all_proxy", "socks5://$proxyServer")
         [Environment]::SetEnvironmentVariable("no_proxy", $no_proxy)
-        # git config --global http.proxy "http://$proxyServer"
-        # git config --global https.proxy "http://$proxyServer"
+        git config --global http.proxy "http://$proxyServer"
+        git config --global https.proxy "http://$proxyServer"
     }
 }
 
@@ -19,12 +19,14 @@ function noproxy {
     [Environment]::SetEnvironmentVariable("https_proxy", $null)
     [Environment]::SetEnvironmentVariable("all_proxy", $null)
     [Environment]::SetEnvironmentVariable("no_proxy", $null)
-    # git config --global --unset http.proxy
-    # git config --global --unset https.proxy
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
 }
 
 if ($proxyEnable) {
     proxy
+} else {
+    noproxy
 }
 
 $exportModuleMemberParams = @{
